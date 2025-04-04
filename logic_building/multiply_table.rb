77a0049acfb9584bb2cb3fede4edeb34674c79d4
dongvv-2538 <vu.viet.dong@sun-asterisk.com>
2025-04-05 06:08:34 +0700

    add multiply_table

---
+++ logic_building/multiply_table.rb
@@ -0,0 +1,19 @@
+
+def print_multiply_table n
+  puts "*" * 20
+
+  for i in 1..10 do
+    puts "#{n}  *  #{i} = #{n * i}"
+  end
+end
+
+print_multiply_table 1
+print_multiply_table 2
+print_multiply_table 3
+print_multiply_table 4
+print_multiply_table 5
+print_multiply_table 6
+print_multiply_table 7
+print_multiply_table 8
+print_multiply_table 9
+print_multiply_table 10
